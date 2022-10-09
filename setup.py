e5e737b296753ca97074c0b980f5ca323c9a8f22
Christophe-Marie Duquesne <chmd@chmd.fr>
2022-10-10 00:44:05 +0200

    following https://python-packaging.readthedocs.io

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='papier',
+    version='0.0.1',
+    description='pdf library organizer',
+    url='https://github.com/chmduquesne/papier',
+    author='Christophe-Marie Duquesne',
+    author_email='chmd+papier@chmd.fr',
+    license='MIT',
+    packages=['papier'],
+    zip_safe=False)
