ff4248f8a029971dd9127a7d8fe774da48e137c6
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2022-10-14 09:28:21 +0200

    Adding support for requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import pathlib
 from setuptools import setup
 
-setup(name='papier',
+here = pathlib.Path(__file__).parent.resolve()
+
+install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
+
+setup(
+    name='papier',
     version='0.0.1',
     description='pdf library organizer',
+    install_requires=install_requires,
     url='https://github.com/chmduquesne/papier',
     author='Christophe-Marie Duquesne',
     author_email='chmd+papier@chmd.fr',
@@ -11,4 +18,5 @@ setup(name='papier',
     entry_points = {
         'console_scripts': ['papier=papier.cli:main'],
     },
-    zip_safe=False)
+    zip_safe=False
+    )
