3bff8706e9eb34b3a28b025ac03dc50c44babfce
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2022-10-18 17:12:02 +0200

    showing how to add a command to papier via plugin

--- papier/plugin/example.py
+++ papier/plugin/example.py
@@ -1,9 +1,16 @@
 """example papier plugin"""
 import papier.plugins
+from papier.cli.commands import command, add_argument
 
 
 def on_plugins_loaded():
     print('all plugins loaded')
+
+
+@command(add_argument('-f', '--flag', action='store_true'))
+def example(args):
+    """help for example command"""
+    print(f'My arguments are {args}')
 
 
 papier.plugins.register_listener('plugins loaded', on_plugins_loaded)
