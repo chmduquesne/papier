7404f805de683af7b9706ffb35aa85270d243b4f
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2022-10-25 17:40:05 +0200

    Adding command line options

--- papier/plugin/importer.py
+++ papier/plugin/importer.py
@@ -1,6 +1,8 @@
 import os
 import re
+import argparse
 from papier.cli.commands import command, add_argument
+import papier
 
 
 
@@ -23,7 +25,28 @@ def process(path):
 
 
 
-@command(add_argument('path', help='path to import'), command_name='import')
+@command(
+        add_argument('path', help='path to import'),
+        add_argument('--copy', action=argparse.BooleanOptionalAction,
+            help=f'Copy files to the library directory after import',
+            default=argparse.SUPPRESS),
+        add_argument('--move', action=argparse.BooleanOptionalAction,
+            help=f'Move files to the library directory after import',
+            default=argparse.SUPPRESS),
+        add_argument('--write', action=argparse.BooleanOptionalAction,
+            help=f'Write tags to any file copied/moved to the library after import',
+            default=argparse.SUPPRESS),
+        add_argument('--ocr', action=argparse.BooleanOptionalAction,
+            help=f'Run OCR on files prior to import if no text is embedded',
+            default=argparse.SUPPRESS),
+        add_argument('--redo-ocr', action=argparse.BooleanOptionalAction,
+            help=f'Run OCR on files prior to import in any case',
+            default=argparse.SUPPRESS),
+        add_argument('--autotag', action=argparse.BooleanOptionalAction,
+            help=f'Automatically tag the files',
+            default=argparse.SUPPRESS),
+        command_name='import')
 def run(args):
+    papier.config['import'].set_args(args)
     for p in find_pdfs(args.path):
         process(p)
