0c4b13445c261282db6601f908291fba8a4977f8
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2022-10-25 09:43:47 +0200

    Filtering for pdfs only

--- papier/plugin/importer.py
+++ papier/plugin/importer.py
@@ -1,19 +1,29 @@
 import os
+import re
 from papier.cli.commands import command, add_argument
+
+
+
+PDF = re.compile(r'.*\.pdf', re.IGNORECASE)
+
+
+
+def find_pdfs(path):
+    if os.path.isfile(path):
+        if PDF.match(path):
+            yield path
+    elif os.path.isdir(path):
+        for p in os.listdir(path):
+            yield from find_pdfs(os.path.join(path,p))
+
+
+
+def process(path):
+    print(path)
 
 
 
 @command(add_argument('path', help='path to import'), command_name='import')
 def run(args):
-    process(args.path)
-
-
-
-def process(path):
-    if os.path.isfile(path):
-        print(path)
-    elif os.path.isdir(path):
-        for file in os.listdir(path):
-            process(os.path.join(path, file))
-    else:
-        pass
+    for p in find_pdfs(args.path):
+        process(p)
