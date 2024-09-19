54e1292d687030b17ad30fe9f0e6b0567c601375
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-09-19 13:37:51 +0200

    plugin/date.py: comply with flake8

---
+++ papier/plugin/date.py
@@ -0,0 +1,35 @@
+import datetime
+import dateparser
+from dateparser_data.settings import default_parsers
+
+
+def predict_date(doc, lang=None, ref=None):
+    import warnings
+    warnings.filterwarnings("ignore", module="dateparser")
+
+    if ref is None:
+        ref = datetime.datetime.now()
+
+    settings = {}
+    settings['REQUIRE_PARTS'] = ['year', 'month']
+    settings['PREFER_DAY_OF_MONTH'] = 'first'
+    settings['PARSERS'] = [p for p in default_parsers if p != 'relative-time']
+
+    languages = None
+    if lang is not None:
+        languages = [lang]
+
+    try:
+        candidates = [(text, date) for (text, date) in
+                      dateparser.search.search_dates(
+                          doc,
+                          languages=languages,
+                          settings=settings) if
+                      date <= ref]
+    except TypeError:
+        candidates = []
+
+    if len(candidates) > 0:
+        return candidates[0][1].strftime("%Y-%m-%d")
+    else:
+        return "XXXX-XX-XX"
