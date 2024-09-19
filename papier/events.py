c1531ba48fc2261bf746a3af87d1315861dfe601
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-09-19 21:39:31 +0200

    papier/events.py: add type annotations

--- papier/events.py
+++ papier/events.py
@@ -1,14 +1,15 @@
 """Support for papier events"""
+from typing import Dict
 
 
 _events = dict()
 
 
-def document_event(name, description):
+def document_event(name: str, description: str) -> None:
     if name in _events:
         raise NameError(f'{name} is already a documented event')
     _events[name] = description
 
 
-def documented_events():
+def documented_events() -> Dict[str, str]:
     return dict(_events)
