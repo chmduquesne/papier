8a141fcb865dbbe660c0c7e62ce7d0badbd0faf9
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-09-19 12:53:59 +0200

    library.py: comply with flake8

--- papier/library.py
+++ papier/library.py
@@ -2,17 +2,16 @@ import papier
 import sqlite3
 
 
-
 def init():
     con = sqlite3.connect(papier.config["library"].get())
     with con as cursor:
-        cur.execute("CREATE TABLE IF NOT EXISTS files("
-            "path TEXT PRIMARY KEY, "
-            "path_orig TEXT, "
-            "mtime REAL, "
-            "content TEXT, "
-            "tags TEXT, "
-            ")")
+        cursor.execute("CREATE TABLE IF NOT EXISTS files("
+                       "path TEXT PRIMARY KEY, "
+                       "path_orig TEXT, "
+                       "mtime REAL, "
+                       "content TEXT, "
+                       "tags TEXT, "
+                       ")")
     return con
 
 
