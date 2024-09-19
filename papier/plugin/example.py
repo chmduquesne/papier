296d9a16628d69b1edb5a51d05158637b95de466
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-09-19 21:29:31 +0200

    plugin/example.py: add type annotations

--- papier/plugin/example.py
+++ papier/plugin/example.py
@@ -1,14 +1,15 @@
 """example papier plugin"""
 import papier.plugins
 from papier.cli.commands import command, add_argument
+from typing import List, Any
 
 
-def on_plugins_loaded():
+def on_plugins_loaded() -> None:
     print('all plugins loaded')
 
 
 @command(add_argument('-f', '--flag', action='store_true'))
-def example(args):
+def example(args: List[Any]) -> None:
     """help for example command"""
     print(f'My arguments are {args}')
 
