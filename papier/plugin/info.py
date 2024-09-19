6a5b80d006d40d5cf592595f8c209fc319c2d8a1
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-09-19 14:48:33 +0200

    plugin/info.py: Add type annotations

--- papier/plugin/info.py
+++ papier/plugin/info.py
@@ -1,15 +1,16 @@
 """plugin to show basic info"""
 import papier
 from papier.cli.commands import command
+from typing import List
 
 
 @command()
-def showconf(args):
+def showconf(args: List[...]) -> None:
     """show the loaded configuration"""
     print(papier.config.dump())
 
 
 @command()
-def version(args):
+def version(args: List[...]) -> None:
     """show the installed version"""
     print(papier.__version__)
