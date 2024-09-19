82304e01fc5e20ec8bf04cdf62115481c81afcbc
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-09-19 13:00:27 +0200

    plugins.py: comply with flake8

--- papier/plugins.py
+++ papier/plugins.py
@@ -6,7 +6,6 @@ import importlib
 from collections import defaultdict
 
 
-
 # Global logger
 log = logging.getLogger('papier')
 
@@ -15,21 +14,18 @@ log = logging.getLogger('papier')
 _listeners = defaultdict(list)
 
 
-
 def register_listener(event, func):
     """Sets func to be called whenever the event is triggered"""
     _listeners[event].append(func)
-
 
 
 def send(event, *args, **kwds):
     """Call all functions registered for the event"""
     log.info(f'Sending event: {event}')
     for func in _listeners[event]:
-        log.info(f'Calling function: {func.__name__}(args={args}, kwds={kwds})')
+        log.info(f'Calling function: '
+                 f'{func.__name__}(args={args}, kwds={kwds})')
         func(*args, **kwds)
-
-
 
 
 def predict_metadata(document, set_tags=None):
@@ -38,14 +34,16 @@ def predict_metadata(document, set_tags=None):
     pass
 
 
-
 def load_plugins(plugins=()):
     for plugin in plugins:
         modname = f'papier.plugin.{plugin}'
         try:
             log.info(f'loading plugin {plugin}')
-            module = importlib.import_module(modname)
-        except ModuleNotFoundError as exc:
+            importlib.import_module(modname)
+        except ModuleNotFoundError:
             log.warning(f'** plugin {plugin} not found')
         except Exception:
-            log.warning(f'** error loading plugin "{plugin}":\n{traceback.format_exc()}')
+            log.warning(
+                    f'** error loading plugin "{plugin}":\n'
+                    f'{traceback.format_exc()}'
+                    )
