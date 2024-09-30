31093b4404ed6c60998dc6386e0051ebe7b28d50
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-09-30 17:15:30 +0200

    Add spacy dependency

--- papier/plugin/lang.py
+++ papier/plugin/lang.py
@@ -11,10 +11,7 @@ fasttext.FastText.eprint = lambda x: None
 @papier.extractor(produces=['lang'])
 def extract_lang(document: papier.Document, tags: Dict[str, Any]
                  ) -> Dict[str, Any]:
-    try:
-        nlp = spacy.load('en_core_web_sm')
-        nlp.add_pipe('language_detector')
-        doc = nlp(document.text)
-        return {'lang': doc._.language}
-    except OSError:
-        spacy.cli.download('en_core_web_sm')
+    nlp = spacy.load('en_core_web_sm')
+    nlp.add_pipe('language_detector')
+    doc = nlp(document.text)
+    return {'lang': doc._.language}
