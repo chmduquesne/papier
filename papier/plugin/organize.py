93d08f8ebaed2d0985803f3c1de89dabab5652ce
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-10-01 18:51:31 +0200

    add an organize plugin

---
+++ papier/plugin/organize.py
@@ -0,0 +1,23 @@
+import papier
+# from papier.cli.commands import command, add_argument
+import jinja2
+
+
+def desired_path(document: papier.Document, tags: dict[str, str]) -> str:
+    env = jinja2.Environment()
+
+    rules = papier.config['organize'].get(list)
+    for rule in rules:
+        when = rule['when']
+        conditions = []
+        for statement in when:
+            rendered = env.from_string(statement).render(**tags)
+            conditions.append(rendered == 'True')
+        if all(conditions):
+            path = rule['path']
+            return env.from_string(path).render(**tags)
+    raise Exception('could not match the document with a rule')
+
+
+def organize(document: papier.Document, tags: dict[str, str]) -> None:
+    pass
