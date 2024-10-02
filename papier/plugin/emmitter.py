0432dec586d5d037193a874e71f7cc4d253e7a47
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-10-02 13:48:07 +0200

    remove irrelevant message from huggingface transformers

--- papier/plugin/emmitter.py
+++ papier/plugin/emmitter.py
@@ -1,6 +1,12 @@
 import papier
 from typing import Any
 import spacy
+import logging
+
+
+# Get rid of the annoying message about padding to max length from
+# transformers/tokenization_utils_base.py
+logging.getLogger('transformers.tokenization_utils_base').setLevel(logging.CRITICAL)
 
 
 @papier.extractor(consumes=['lang'], produces=['emmitter'])
