3166e77f9a9a0390568157a9d8edbe0af3013f11
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-10-02 15:29:06 +0200

    remove the messages from huggingface

--- papier/plugin/emmitter.py
+++ papier/plugin/emmitter.py
@@ -2,11 +2,15 @@ import papier
 from typing import Any
 import spacy
 import logging
+import huggingface_hub
 
 
-# Get rid of the annoying message about padding to max length from
-# transformers/tokenization_utils_base.py
-logging.getLogger('transformers.tokenization_utils_base').setLevel(logging.CRITICAL)
+# Get rid of the annoying message from transformers/tokenization_utils_base.py
+logging.getLogger('transformers.tokenization_utils_base').setLevel(
+        logging.CRITICAL)
+
+# Get rid of the download progress bars
+huggingface_hub.utils.disable_progress_bars()
 
 
 @papier.extractor(consumes=['lang'], produces=['emmitter'])
