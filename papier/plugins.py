b6957465f5002475893a0659abef9e7d031a9063
Christophe-Marie Duquesne <chmd+papier@chmd.fr>
2024-10-09 09:05:49 +0200

    correct type

--- papier/plugins.py
+++ papier/plugins.py
@@ -20,7 +20,7 @@ def register_listener(event: str, func: Callable) -> None:
     _listeners[event].append(func)
 
 
-def send(event: str, *args: Tuple[Any, ...], **kwds: Dict[str, Any]) -> None:
+def send(event: str, *args: Tuple[Any], **kwds: Dict[str, Any]) -> None:
     """Call all functions registered for the event"""
     log.info(f'Sending event: {event}')
     for func in _listeners[event]:
